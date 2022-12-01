d252a31e01f2a2fcd6eb0d2c2379a4f33991e4e7
Adrián <adrianlomu2004@gmail.com>
2022-12-01 17:09:58 +0100

    presentation(importJson.py): importa la información del archivo JSON

---
+++ src/presentation/importJson.py
@@ -0,0 +1,17 @@
+# Importamos la libreria de JSON
+import json
+
+
+# Creamos una funcion que nos sacara la información del archivo JSON y metera todo en la variable bicis
+def importarDatos():
+    
+    # Abrimos el archivo JSON en modo de lectura y cargamos los datos en la variable "bicicletas"
+    with open("../monbike/src/database/bicis.json", encoding="utf-8") as datos:
+        bicicletas = datos.read()
+
+        # Cargar los datos de la variable "bicicletas" en una variable llamada "bicis"
+        bicis = json.loads(bicicletas)
+        return bicis
+
+
+importarDatos()

1a088ceba34224c31e5a17d7fa4e1b614d65dcc0
Adrián López <adrianlomu2004@gmail.com>
2022-12-04 20:19:59 +0100

    presentation(variablesHtml): agregado variables Python con estructura/codigo de HTML

---
+++ src/presentation/variablesHtml.py
@@ -0,0 +1,37 @@
+
+# Variables globales HTML en Python
+head = f'''<!Doctype html>
+<html LANG="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Alquila la bici que mas te guste al mejor precio">
+        <meta name="author" content="Adrián López">
+        <meta name="date" content="27/11/2020">
+        <meta name="last-modified" content="04 de Diciembre de 2022">
+        <meta name="keyword" content="Bicis, Alquilar bicis, Monbike, Mallorca bicis">
+        <link rel="StyleSheet" href="style.css">
+        
+        <!-- LOGO MonBike-->
+        <link rel="shortcut icon" href="img/monbike_bici_logo.png">
+
+        <!-- Titulo MonBike -->
+        <title>MonBike</title>
+    </head>\n''' 
+
+body = "    <body>\n"
+fin_body = "    </body>\n"
+
+br = "<br>\n"
+
+footer = f'''<footer id="main-footer">
+            <p>&copy; 2022 por Adrián López</p>
+        </footer>\n'''
+
+
+# Variables para la pagina donde aparece todas las bicis
+
+div_global = '<div class="general_bicis">\n'
+general_unica_bici = '<div class="general_una_bici">\n'
+fin_div = '</div>\n'
+
+fin_html = "</html>"

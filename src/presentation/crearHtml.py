8f59c540d6aab2fc521097abb7cebcbfde951976
Adrián López <adrianlomu2004@gmail.com>
2022-12-04 18:39:17 +0100

    presentation(crearHtml.py): agregado un mini bucle para printar las bicis con sus nombre, imagenes y un enlace

--- src/presentation/crearHtml.py
+++ src/presentation/crearHtml.py
@@ -5,7 +5,7 @@ from importJson import importarDatos
 bicis = importarDatos()
 
 
-# Aqui ejecutamos el codigo que nos crea/sobre escribe "index.html" en el directorio "docs" y nos pone la siguiente estructura:
+# Aqui ejecutamos el codigo que nos crea/sobre escribe "index.html" en el directorio "docs" y le ponemos la siguiente estructura HTML:
 index_html = open("../monbike/docs/index.html", "w")
 
 index_html.write("<!DOCTYPE html>\n")
@@ -18,10 +18,13 @@ index_html.write("</head>\n")
 
 index_html.write("<body>\n")
 
+# Creo una pequeña prueba para que escriba todos los nombres con sus respectivas imagenes y futuros enlaces
 i = 0
 for bici in bicis:
     index_html.write("<h1>"+ bicis[i]["nombre"] +"</h1>\n")
     index_html.write("<img src='"+ bicis[i]["img"] +"'>\n")
+    index_html.write("<a href='"+ bicis[i]["_id"] +".html'>ALQUILAR</a>\n")
+
     i += 1
 
 index_html.write("</body>\n")

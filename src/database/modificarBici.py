4f5cfc890c0ee751ab528fd3ba38c6a1c8a5fe95
Adrián López <adrianlomu2004@gmail.com>
2022-12-11 16:05:00 +0100

    enhancement(modificarBici.py): implementada funcion que modifica la bici indicada

---
+++ src/database/modificarBici.py
@@ -0,0 +1,52 @@
+# Importamos las librerias necesarias para la API de MongoDB
+import requests
+import json
+import os
+
+
+
+def modificarBici():
+
+    # Preguntamos la información necesaria
+    idBici = input("Indica el ID de la bici que quieres modificar: ")
+    campoBici = input("Que campo de la bici quieres editar? ")
+    valorBici = input("Que valor quieres que sea el nuevo? ")
+
+    # Llamamos desde la variable de entorno la URL de la API
+
+    MONGO_URL = os.environ['MONGO_URL']
+    API_KEY = os.environ['API_KEY']
+
+    # Modificamos la URL de la API y añadimos "updateOne"
+    url = MONGO_URL+"/updateOne"
+
+
+    # Le indicamos las caracteristicas que vamos a utilizar
+    payload = json.dumps({
+        "collection": "bicicletas",
+        "database": "monbike",
+        "dataSource": "MonBike",
+        "filter":{ "_id": { "$oid": idBici } },
+      "update": {
+          "$set": {
+              campoBici: valorBici
+          }
+      }
+    })
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': API_KEY, 
+    }
+
+    print("Bicicleta modificada con existo!")
+
+    modificarOpcion = input("Deseas modificar algo mas? (S)/(N)")
+
+    if modificarOpcion == "S":
+        modificarBici()
+    
+    else:
+        response = requests.request("POST", url, headers=headers, data=payload,)
+        

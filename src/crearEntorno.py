ace9448a6b1c72598024e39fdbd146c4fc032031
Adrián López <adrianlomu2004@gmail.com>
2022-12-11 13:12:37 +0100

    enhancement(crearEntorno.py): añadid dos nuevas funciones al crear el entorno, primero elimina todo el entorno por prevención y despues te crea todos los archivos necesarios

--- src/crearEntorno.py
+++ src/crearEntorno.py
@@ -3,6 +3,13 @@
 # Importamos la libreria OS
 import os
 
+# Importamos la función rmtree que nos permite eliminar carpetas con contenido
+from shutil import rmtree
+
+
+def eliminarEntorno(ruta):
+    # Eliminamos el entorno por seguridad
+    rmtree(ruta)
 
 def creadorRutas(ruta):
     # Crear carpetas
@@ -23,6 +30,10 @@ def creadorArchivos(archivo):
 # Funcion que crea el entorno necesario
 def creadorEntornoNecesario():
 
+    # Eliminamos la carpeta docs por seguridad
+    eliminarEntorno("..\monbike\docs")
+    print("Entorno eliminado con exito!")
+
     # Crear carpetas
     creadorRutas("..\monbike\docs")
 
@@ -30,6 +41,37 @@ def creadorEntornoNecesario():
     # Crear archivos HTML
     creadorArchivos("..\monbike\docs\index.html")
     creadorArchivos("..\monbike\docs\general.html")
+    creadorArchivos("..\monbike\docs\contacto.html")
+
+    # Crea archivos HTML de las Ubicaciones
+    creadorArchivos("..\monbike\docs\Alcúdia.html")
+    creadorArchivos("..\monbike\docs\Artá.html")
+    creadorArchivos("..\monbike\docs\Felanix.html")
+    creadorArchivos("..\monbike\docs\Inca.html")
+    creadorArchivos("..\monbike\docs\Manacor.html")
+    creadorArchivos("..\monbike\docs\Marratxi.html")
+    creadorArchivos("..\monbike\docs\Palma.html")
+    creadorArchivos("..\monbike\docs\Pollensa.html")
+    creadorArchivos("..\monbike\docs\Soller.html")
+
+    # Crea archivos HTML de las marcas
+    creadorArchivos("..\monbike\docs\Deporvillage.html")
+    creadorArchivos("..\monbike\docs\Elops.html")
+    creadorArchivos("..\monbike\docs\Focus.html")
+    creadorArchivos("..\monbike\docs\Motion.html")
+    creadorArchivos('../monbike/docs/NCM.html')
+    creadorArchivos("..\monbike\docs\Riverside.html")
+    creadorArchivos("..\monbike\docs\Rockrider.html")
+    creadorArchivos("..\monbike\docs\SHIMANO.html")
+    creadorArchivos("..\monbike\docs\TRIBAN.html")
+    creadorArchivos("..\monbike\docs\VAN RYSEL.html")
+    creadorArchivos("..\monbike\docs\Windee.html")
+
+    # Crea archivos HTML de los tipos
+    creadorArchivos("..\monbike\docs\carretera.html")
+    creadorArchivos("..\monbike\docs\city.html")
+    creadorArchivos("..\monbike\docs\eléctrica.html")
+    creadorArchivos("..\monbike\docs\MTB.html")
         
     # Crear archivos CSS
     creadorArchivos("..\monbike\docs\style.css")
